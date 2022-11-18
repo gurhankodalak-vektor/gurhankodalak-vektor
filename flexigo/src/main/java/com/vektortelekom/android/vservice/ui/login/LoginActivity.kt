e07f25b2c0197c6230177bc0c848071f44f798f8
sedaAytekin <seda.aytekin@vektortelekom.com>
2022-11-18 16:04:24 +0300

    login page delete token

--- flexigo/src/main/java/com/vektortelekom/android/vservice/ui/login/LoginActivity.kt
+++ flexigo/src/main/java/com/vektortelekom/android/vservice/ui/login/LoginActivity.kt
@@ -30,9 +30,9 @@ class LoginActivity : BaseActivity<LoginViewModel>(), LoginNavigator  {
 
         binding = DataBindingUtil.setContentView<LoginActivityBinding>(this, R.layout.login_activity).apply {
             lifecycleOwner = this@LoginActivity
-            viewModel = this@LoginActivity.viewModel
         }
         viewModel.navigator = this
+        stateManager.logout()
 
         viewModel.langCode = resources.configuration.locale.language
 
